f09276abb9bbd282ef6a73110645d2c768587807
Georgi Popov <georgi.k.popovv@gmail.com>
2026-07-30 13:52:11 +0300

    Pass key directly to JSX instead of spreading it (React warning)

--- src/timeline-list/index.js
+++ src/timeline-list/index.js
@@ -94,7 +94,6 @@ const TimelineList = props => {
       const numberOfDaysToDrop = 7 - numberOfDays;
       const _timelineProps = {
         ...timelineProps,
-        key: item,
         date: dropRight(weekDates, numberOfDaysToDrop),
         events: flatten(dropRight(weekEvents, numberOfDaysToDrop)),
         scrollToNow: _isToday && isInitialPage && scrollToNow,
@@ -111,7 +110,7 @@ const TimelineList = props => {
       }
       return (
         <>
-          <Timeline {..._timelineProps} />
+          <Timeline key={item} {..._timelineProps} />
           {/* NOTE: Keeping this for easy debugging */}
           {/* <Text style={{position: 'absolute'}}>{item}</Text>*/}
         </>
